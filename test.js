d20f5f0e99bbfe909e85d71db0f6abf07783368b
Carrie <38140275+cscottrun@users.noreply.github.com>
2019-01-15 12:00:57 -0600

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const sort = require('./sort')
 
 describe('Quick Sort', () => {
   let arr1 = [4,2,6,8,9,1]
-  let arr2 = [12, 11, 13, 5, 6, 7]
+  let arr2 = [1,4,7,9,2,8,3,6]
   let arr3 = [40,30,55,82,34,44]
 
   it('should return array sorted', () => {
@@ -20,9 +20,9 @@ describe('Heap Sort', () => {
   let arr3 = [40,30,55,82,34,44]
 
   it('should return array sorted', () => {
-    expect(sort.quick(arr)).to.eql(arr.sort())
-    expect(sort.quick(arr2)).to.eql(arr2.sort())
-    expect(sort.quick(arr3)).to.eql(arr3.sort())
+    expect(sort.heap(arr)).to.eql(arr.sort())
+    expect(sort.heap(arr2)).to.eql(arr2.sort())
+    expect(sort.heap(arr3)).to.eql(arr3.sort())
 
   })
 })
